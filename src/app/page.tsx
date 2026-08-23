8165497c29957cea2c10c03d7ea7aad91f60b8ea
Ifeoluwa Sulaiman <ifeoluwasulaiman30@gmail.com>
2026-08-23 12:23:05 +0100

    init: project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <main>
-      <div className="text-5xl underline">Welcome to Next.js</div>
+      <div className="text-5xl underline">Welcome to the Animated Website</div>
     </main>
   );
 };
